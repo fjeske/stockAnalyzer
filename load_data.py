b52787364131626bec0c552190711e110ef0e109
Fabian Jeske <fabianjeske@Air-von-Fabian.fritz.box>
2021-12-19 23:08:15 +0100

    added class for loading the data

---
+++ load_data.py
@@ -0,0 +1,16 @@
+"""Class for loading stock data"""
+import yfinance as yf
+from datetime import datetime
+
+class DataLoader:
+    def __init__(self):
+        self.today = datetime.today().strftime('%Y-%m-%d')
+
+    def load_data(self, ticker, start, end=None, period='1d'):
+        self.ticker = ticker
+        self.start = start
+        if end is None:
+            self.end = datetime.today().strftime('%Y-%m-%d')
+        elif end is not None:
+            self.end = end
+        self.period = period
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """
 This is the main sctipt for the Stock-Analyzer
 """
+from load_data import DataLoader
 
 def main():
     print("Hello fellow stock enthusiast!")
+    data = DataLoader()
+    msft = data.load_data(ticker='MSFT', start='2021-1-1')
+    print(msft)
+
 
 if __name__ == "__main__":
     main()
